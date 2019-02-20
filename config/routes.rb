865908f5c0aa25820be81fc15eac952f7dfa7998
takuma watanabe <takuma_0920@docomo.ne.jp>
2019-02-20 13:04:24 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :tasks
   root to: "tasks#index"
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  get "signup",to: "users#new"
+  resources :tasks
+  resources :users, only:[:index,:show,:new,:create]
 end
